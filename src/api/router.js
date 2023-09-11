022b37563083cd3b5ec8850af9c967b21a82957d
sergiomll1968 <sergiomaciaslluch@gmail.com>
2023-09-11 16:40:29 +0200

    act 20230911 1640

--- src/api/router.js
+++ src/api/router.js
@@ -2,22 +2,22 @@ import Router from 'express';
 
 import awardsRouter from './awards/awards.router.js';
 import bookingsRouter from './bookings/bookings.router.js';
-import servicesRouter from './services/services.router.js';
+import servicesRouter from './sites/services.router.js';
 import usersRouter from './users/users.router.js';
 
 import * as authController from './auth/auth.controller.js';
 
 const router = Router();
 
-// router.use('/awards', awardsRouter);
-// router.use('/bookings', bookingsRouter);
-// router.use('/services', servicesRouter);
-// router.use('/users', usersRouter);
+router.use('/awards', awardsRouter);
+router.use('/bookings', bookingsRouter);
+router.use('/services', servicesRouter);
+router.use('/users', usersRouter);
 
-// router.post('/register', authController.register);
+router.post('/register', authController.register);
 router.post('/login', authController.login);
 
-// router.get('/confirm/:emailtoken', authController.confirm);
+router.get('/confirm/:emailtoken', authController.confirm);
 
 router.get('/test', (req, res) => {
   res.send('Desafio tripulaciones!');
